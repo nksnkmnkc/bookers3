8f8ddf4bfdeea8e8215406d9f06e06a4f68c24c5
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2023-01-20 07:11:50 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
   devise_for :users
+  
+  root to: "homes#top"
+  get '/home/about' => "homes#about", as: "about"
+  
+  resources :books, only:[:new, :index, :show, :edit , :destroy, :update, :create]
+  resources :users, only:[:show, :edit, :index ,:update, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
